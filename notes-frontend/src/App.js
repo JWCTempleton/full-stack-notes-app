f2a4d3ea964095639c57e4a62d4cf4fbe9318ce0
Jacob Templeton <jacob.templeton@gmail.com>
2023-06-10 18:46:14 -0700

    Add first concept notes array, display first concept notes on page styled with MUI

--- notes-frontend/src/App.js
+++ notes-frontend/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  height: 80,
+  lineHeight: "60px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
 function App() {
+  const notes = [
+    {
+      id: 1,
+      content: "Notes app working",
+      important: true,
+    },
+    {
+      id: 2,
+      content: "React app",
+      important: false,
+    },
+    {
+      id: 3,
+      content: "GET and POST are important methods of HTTP protocol",
+      important: true,
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        sx={{
+          p: 3,
+          bgcolor: "background.default",
+          display: "flex",
+          flexDirection: "column",
+          width: "50vw",
+          gap: 2,
+          margin: "0 auto",
+        }}
+      >
+        {notes.map((note) => {
+          return (
+            <Item key={note.id} elevation={6}>
+              {`${note.content}`}
+            </Item>
+          );
+        })}
+      </Box>
     </div>
   );
 }
