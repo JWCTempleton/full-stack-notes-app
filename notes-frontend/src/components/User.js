54c14eea089556708d43593866122de35590cea0
Jacob Templeton <jacob.templeton@gmail.com>
2023-07-26 16:31:41 -0700

    Add Typography for user name on User page and a total of the notes created by the user based on the return data from the GET request

--- notes-frontend/src/components/User.js
+++ notes-frontend/src/components/User.js
@@ -1,5 +1,6 @@
 import { userService } from "../services/user";
 import { useQuery } from "react-query";
+import { Box, Typography, CircularProgress } from "@mui/material";
 
 const User = ({ user }) => {
   const result = useQuery(
@@ -9,8 +10,37 @@ const User = ({ user }) => {
       refetchOnWindowFocus: false,
     }
   );
+  if (result.isLoading) {
+    return (
+      <Box
+        sx={{
+          width: "100vw",
+          height: "60px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingTop: "20px",
+        }}
+      >
+        <Typography>Loading...</Typography>
+        <CircularProgress />
+      </Box>
+    );
+  }
   console.log("USER BACKEND", result);
-  return <div>User</div>;
+  return (
+    <div>
+      <Box>
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          {user.username}
+        </Typography>
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+          Total notes: {result.data.length}
+        </Typography>
+      </Box>
+    </div>
+  );
 };
 
 export default User;
