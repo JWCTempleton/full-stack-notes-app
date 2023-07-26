cd187516f696f3a5565a3948c1ecfb8af3286ecf
Jacob Templeton <jacob.templeton@gmail.com>
2023-07-26 16:46:44 -0700

    Add Note component to render notes, ability to change importance and delete notes

--- notes-frontend/src/components/User.js
+++ notes-frontend/src/components/User.js
@@ -1,8 +1,13 @@
+import { useMutation, useQuery } from "react-query";
 import { userService } from "../services/user";
-import { useQuery } from "react-query";
-import { Box, Typography, CircularProgress } from "@mui/material";
+import { Box, Typography, CircularProgress, Button } from "@mui/material";
+import { noteService } from "../services/notes";
+import { useState } from "react";
+import Note from "./Note";
 
-const User = ({ user }) => {
+const User = ({ user, queryClient }) => {
+  const [showAll, setShowAll] = useState(true);
+
   const result = useQuery(
     "userNotes",
     () => userService.getAllUserNotes(user),
@@ -10,6 +15,45 @@ const User = ({ user }) => {
       refetchOnWindowFocus: false,
     }
   );
+
+  const newNoteMutation = useMutation(noteService.create, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData("userNotes");
+      queryClient.setQueryData(
+        "userNotes",
+        notes.concat([{ ...newNote[0], username: user.username }])
+      );
+    },
+  });
+
+  const updateNoteMutation = useMutation(noteService.update, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData("userNotes");
+      queryClient.setQueryData(
+        "userNotes",
+        notes.map((note) =>
+          note.note_id !== newNote[0].note_id
+            ? note
+            : { ...newNote[0], username: user.username }
+        )
+      );
+    },
+  });
+
+  const deleteNoteMutation = useMutation(noteService.remove, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData("userNotes");
+      queryClient.setQueryData(
+        "userNotes",
+        notes.filter((n) => n.note_id !== newNote[0].note_id)
+      );
+    },
+  });
+
+  const handleShowImportance = () => {
+    setShowAll(!showAll);
+  };
+
   if (result.isLoading) {
     return (
       <Box
@@ -38,6 +82,38 @@ const User = ({ user }) => {
         <Typography variant="h6" sx={{ fontWeight: "bold" }}>
           Total notes: {result.data.length}
         </Typography>
+      </Box>
+      <Box
+        sx={{
+          p: 3,
+          bgcolor: "background.default",
+          display: "flex",
+          flexDirection: "column",
+          width: "65vw",
+          gap: 3,
+          margin: "0 auto",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleShowImportance}
+          sx={{ alignSelf: "center" }}
+        >
+          {showAll ? "Important" : "All"}
+        </Button>
+        {result.data.map((note) => {
+          return (
+            <Note
+              key={note.note_id}
+              note={note}
+              user={user}
+              queryClient={queryClient}
+              updateNoteMutation={updateNoteMutation}
+              deleteNoteMutation={deleteNoteMutation}
+            />
+          );
+        })}
       </Box>
     </div>
   );
